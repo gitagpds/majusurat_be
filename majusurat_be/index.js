acc12a1650c0f2cce35a6f018d32aaae0fd28b62
Jeslyn V <162588624+midnightwalks@users.noreply.github.com>
2025-06-11 10:31:06 +0700

    Update index.js

--- majusurat_be/index.js
+++ majusurat_be/index.js
@@ -86,7 +86,7 @@ app.use((req, res) => {
 });
 
 // Jalankan server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
